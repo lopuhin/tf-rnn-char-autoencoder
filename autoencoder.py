21ffc562beda3fd42f62902105fb6c67fd453f53
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2015-12-12 15:50:21 +0300

    add loss, do proper training

--- autoencoder.py
+++ autoencoder.py
@@ -15,6 +15,7 @@ PAD_ID, GO_ID = _RESERVED = 0, 1
 
 
 def main():
+    random.seed(1)
     parser = argparse.ArgumentParser()
     arg = parser.add_argument
     arg('filename')
@@ -28,8 +29,10 @@ def main():
     input_size = len(char_to_id)
     print 'input_size', input_size
 
-    encoder_inputs, decoder_inputs, encoder_outputs, encoder_states = \
+    encoder_inputs, decoder_inputs, decoder_outputs, decoder_loss = \
         _create_model(input_size, args)
+    optimizer = tf.train.AdamOptimizer()
+    train_op = optimizer.minimize(decoder_loss)
     with tf.Session() as sess:
         sess.run(tf.initialize_all_variables())
         for step in xrange(args.n_steps):
@@ -40,21 +43,29 @@ def main():
                 var.name: val for var, val in
                 chain(izip(encoder_inputs, batch_inputs),
                       izip(decoder_inputs, batch_outputs))}
-            # TODO - loss?
-            outputs = sess.run(encoder_outputs, feed_dict)
+            _, loss = sess.run([train_op, decoder_loss], feed_dict)
+            if step % 100 == 1:
+                print step, loss
 
 
 def _create_model(input_size, args):
-    cell = rnn_cell.LSTMCell(args.state_size, input_size)
+    cell = rnn_cell.LSTMCell(args.state_size, input_size, num_proj=input_size)
     encoder_inputs, decoder_inputs = [[
         tf.placeholder(tf.float32, shape=[args.batch_size, input_size],
                        name='{}{}'.format(name, i))
         for i in xrange(length)] for name, length in [
             ('encoder', args.max_seq_length),
             ('decoder', args.max_seq_length)]]
-    outputs, states = seq2seq.tied_rnn_seq2seq(
+    decoder_outputs, _ = seq2seq.tied_rnn_seq2seq(
         encoder_inputs, decoder_inputs, cell)
-    return encoder_inputs, decoder_inputs, outputs, states
+    params = tf.trainable_variables()
+    # TODO - add weights
+    targets = decoder_inputs[1:]
+    decoder_loss = tf.reduce_mean(tf.add_n([
+        tf.nn.softmax_cross_entropy_with_logits(
+            logits, target, name='seq_loss_{}'.format(i))
+        for i, (logits, target) in enumerate(zip(decoder_outputs, targets))]))
+    return encoder_inputs, decoder_inputs, decoder_outputs, decoder_loss
 
 
 def _read_inputs(filename, max_seq_length):
@@ -101,7 +112,7 @@ def _prepare_batch(inputs, input_size, args):
                 (batch_outputs, [[GO_ID], input_ , repeat(PAD_ID, n_pad - 1)])
                 ]:
             for i, id_ in enumerate(chain(*seq)):
-                values[i][n_batch] = id_
+                values[i][n_batch][id_] = 1.0
     return batch_inputs, batch_outputs
 
 
